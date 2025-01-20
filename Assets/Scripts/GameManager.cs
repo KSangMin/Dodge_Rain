94224d5e797b0129f305cff31f1af47b466b7ac1
KSangMin <fhfpszmfnwm@naver.com>
2025-01-20 21:57:55 +0900

    재시작 기능
    재시작 기능

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,17 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     public GameObject raindrop;
     private int _score = 0;
+    private float _time = 10;
 
     public Text scoreText;
+    public Text TimeText;
+
+    public GameObject endPanel;
 
     private void Awake()
     {
         Instance = this;
+        Time.timeScale = 1.0f;
     }
     // Start is called before the first frame update
     void Start()
@@ -24,7 +29,17 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(_time > 0)
+        {
+            _time -= Time.deltaTime;
+        }
+        else
+        {
+            _time = 0f;
+            endPanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        TimeText.text = _time.ToString("N1");
     }
 
     public void CreateRain()
